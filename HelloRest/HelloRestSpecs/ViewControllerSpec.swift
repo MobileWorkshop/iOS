54a7a5c006c18c2c8293423b6e92d16d958ddecc
Venkat Palivela <venkat.palivela@gmail.com>
2016-06-29 08:28:30 -0500

    Venkat - Added a simple rest client implementation

---
+++ HelloRest/HelloRestSpecs/ViewControllerSpec.swift
@@ -0,0 +1,80 @@
+//
+//  ViewControllerSpec.swift
+//  HelloRest
+//
+//  Created by Venkat on 6/24/16.
+//  Copyright © 2016 Venkat. All rights reserved.
+//
+
+import Quick
+import Nimble
+import SwiftyJSON
+import OHHTTPStubs
+import Alamofire
+@testable import HelloRest
+
+import UIKit
+
+
+class FakeViewController: ViewController {
+    var getDataCalled: Bool = false
+    
+    
+    override func getData() {
+        getDataCalled = true;
+    }
+}
+
+class ViewControllerSpec: QuickSpec {
+    override func spec() {
+        beforeEach { 
+            stub(isHost("localhost") && isPath("/listall") && isMethodGET()) { request in
+                
+                let obj = [["key1":"value1"], ["key2":["value2A","value2B"]]]
+                
+                return OHHTTPStubsResponse(JSONObject: obj, statusCode: 200, headers: nil).responseTime(OHHTTPStubsDownloadSpeed3G)
+                
+            }
+        }
+        
+        afterEach { 
+            OHHTTPStubs.removeAllStubs()
+        }
+        
+        describe(".getData") {
+            it("should get data from list all") {
+                
+                let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+                
+                let vc = storyBoard.instantiateViewControllerWithIdentifier("ViewController") as! ViewController
+                
+                vc.getData();
+                
+                expect(vc.jsonResults).toEventually(haveCount(2))
+            }
+            
+        }
+        
+        describe(".viewDidLoad") {
+            var fakeVC: FakeViewController!
+            
+            beforeEach{
+                fakeVC = FakeViewController()
+                fakeVC.viewDidLoad()
+            }
+            
+            it("should fetch data from server"){
+                expect(fakeVC.getDataCalled).to(beTrue())
+            }
+            
+            it("should setup title and adapter"){
+                expect(fakeVC.title).to(equal("Hello Rest!"))
+                
+                let adapter = fakeVC.tableAdapter
+                expect(adapter).toNot(beNil())
+                
+                expect(adapter?.items).to(haveCount(0))
+            }
+        }
+    }
+}
